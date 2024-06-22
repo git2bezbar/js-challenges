d4cd8f848db66672d90326d5df05eb40f2865b56
Adem <ademduran6888@gmail.com>
2024-06-22 13:51:49 +0200

    feat(routes): adding the app index page

---
+++ packages/note-taking/src/routes/index.lazy.tsx
@@ -0,0 +1,37 @@
+import type { Note } from "../services/types";
+
+import { Link, createLazyFileRoute } from '@tanstack/react-router';
+
+import Card from "../components/Card";
+import { useNoteStore } from "../store/notes";
+
+export const Route = createLazyFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  const { notes } = useNoteStore();
+	notes.sort((a: Note, b: Note) =>
+		(a.updatedAt && b.updatedAt && a.updatedAt < b.updatedAt) ? 1 : -1);
+
+  return (
+    <div className="container">
+			<div className="titleWithButtons">
+      	<h1>Notes</h1>
+				<Link to="/add">
+					<button data-type="primary">Add note</button>
+				</Link>
+			</div>
+			<ul>
+				{
+					notes.length > 0
+						? notes.map((note: Note) =>
+						<li key={note.uuid}>
+							<Card note={note} />
+						</li>)
+						: <li><p>You currently have no notes</p></li>
+				}
+			</ul>
+    </div>
+  )
+}
